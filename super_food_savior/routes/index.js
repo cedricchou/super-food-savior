a7ba600a52900a517b762941aebffd736dff7501
Cedric Chou <cdric.chou@gmail.com>
2018-05-08 20:17:15 -0700

    express uploader configured and working - need to wrap upload form with donation

--- super_food_savior/routes/index.js
+++ super_food_savior/routes/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const fileUpload = require('express-fileupload');
+
+router.use(fileUpload());
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   console.log(req.user);
   console.log(req.isAuthenticated())
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload', function(req, res) {
+  if (!req.files) {
+    return res.send('No files were uploaded');
+  }
+  let donationPic = req.files.donationPic;
+  let donationPicName = donationPic.name;
+  console.log(donationPicName);
+  donationPic.mv('./public/upload/'+donationPicName, function(err) {
+    if (err) {
+      return res.send(err);
+    } else {
+      res.redirect('/donations');
+    }
+  })
+})
 
 module.exports = router;
